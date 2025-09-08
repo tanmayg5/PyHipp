87603627dc4d208d21bbb6b1d10fe867945691be
AWS ParallelCluster user <ec2-user@ip-10-0-12-91.ap-southeast-1.compute.internal>
2025-09-08 04:41:19 +0000

    highpassedit

--- rplhighpass-sort-slurm.sh
+++ rplhighpass-sort-slurm.sh
@@ -14,6 +14,10 @@
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
 
+/data/miniconda3/bin/conda init
+source ~/.bashrc
+envarg=`/data/src/PyHipp/envlist.py`
+conda activate $envarg
 python -u -c "import PyHipp as pyh; \
 import time; \
 pyh.RPLHighPass(saveLevel=1); \
@@ -22,6 +26,5 @@ mountain_batch.mountain_batch(); \
 from PyHipp import export_mountain_cells; \
 export_mountain_cells.export_mountain_cells(); \
 print(time.localtime());"
-
-aws sns publish --topic-arn arn:aws:sns:ap-southeast-1:528424611610:awsnotify --message "RPLHighpassSortJobDone"
-
+conda deactivate 
+/data/src/PyHipp/envlist.py $envarg
